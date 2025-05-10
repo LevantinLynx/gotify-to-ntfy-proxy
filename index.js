67fb9f7ba4761a5648558e6420cfa9ec923e849d
LevantinLynx <git@levantinlynx.com>
2025-05-10 15:50:26 +0000

    updated dependencies, changed to body parser split up functions

--- index.js
+++ index.js
@@ -28,15 +28,15 @@ try {
 }
 const express = require('express')
 const multer = require('multer')
-const bodyParser = require('body-parser')
+const { json } = require('body-parser')
 
 const app = express()
 const upload = multer()
 
 app.use(upload.none())
-app.use(bodyParser.json())
+app.use(json())
 
-app.post('/message', upload.none(), bodyParser.json(), async (req, res) => {
+app.post('/message', upload.none(), json(), async (req, res) => {
   const token = req.query.token || req.headers['x-gotify-key'] || req.headers.authorization ? req.headers.authorization.replace('Bearer ', '') : ''
 
   mainStory.debug('MESSAGE', 'Gotify message recieved:', {
